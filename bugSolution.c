97ed39df59e7956f19278f3c60f07c56ec4b0c84
BugHunter-X <askzenai@gmail.com>
2025-02-03 13:56:05 +0100

    Add bugSolution.c file

---
+++ bugSolution.c
@@ -0,0 +1,15 @@
+int main() {
+  int x = 10;
+  int *ptr = NULL; // Initialize the pointer to NULL
+  ptr = &x;
+  *ptr = 20; 
+  printf("%d", x); // Output: 20 
+
+  int y = 30;
+  ptr = &y; 
+  *ptr = 40; 
+  printf("%d", x); //Output: 20
+  printf("%d", y); // Output: 40
+
+  return 0;
+} 
